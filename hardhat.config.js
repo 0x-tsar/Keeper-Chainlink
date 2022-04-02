2e09e9044c3d044c3ad3e6fe0ce353c65e6ec7f8
Gustavo <contato.gustavofernandes@hotmail.com>
2022-04-02 15:24:52 -0300

    ready

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -31,5 +32,8 @@ module.exports = {
       url: process.env.kovan,
       chainId: 42,
     },
+  },
+  etherscan: {
+    apiKey: "API_ETHRESCAN",
   },
 };
